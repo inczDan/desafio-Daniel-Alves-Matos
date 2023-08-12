3f0d79afebe8192dc8f44231d5668e567161e4ab
inczDan <daniel.alvmatos@gmail.com>
2023-08-12 20:39:21 -0300

    feat: implementa estrutura inicial

---
+++ src/caixa-da-lanchonete.js
@@ -0,0 +1,23 @@
+class CaixaDaLanchonete {
+
+    calcularValorDaCompra(metodoDePagamento, itens) {
+        // const cardapio = {
+        //     cafe : 3.00,
+        //     chantily : 1.50,
+        //     suco : 6.20,
+        //     sanduiche : 6.50,
+        //     queijo : 2.00,
+        //     combo1 : 9.50,
+        //     combo2 : 7.50
+        // }
+        // const formasPagamento = ['dinheiro', 'debito', 'credito']
+        // const desconto = 0.95
+        // const taxa = 1.03
+
+
+        return "";
+    }
+
+}
+
+export { CaixaDaLanchonete };
