4b98bede501a44580d4882ebdd1cd5a011591530
inczDan <daniel.alvmatos@gmail.com>
2023-08-15 11:36:03 -0300

    fix: ajustando identações e nome das variaveis

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -19,60 +19,58 @@ class CaixaDaLanchonete {
         this.taxa = 1.03
         }
         
-        validarItens (itens) {
-        if (itens.length === 0) {
-        return "Não há itens no carrinho de compra!"
-        }
+        validarItensNoCarrinho (itens) {
+          if (itens.length === 0) {
+            return "Não há itens no carrinho de compra!"
+          }
         }
         
         validarProdutos (produto) {
-        if (produto.length < 2 || !(produto in this.cardapio)) {
-        return "Item inválido!"
-        }
+          if (produto.length < 2 || !(produto in this.cardapio)) {
+            return "Item inválido!"
+          }
         }
         
         validarItensExtras (item) {
-        return this.itensExtras.some(itemAtual => itemAtual.item === item)
+          return this.itensExtras.some(itemAtual => itemAtual.item === item)
         }
         
         validarItemPrincipal (item) {
-        const itemExtra = this.itensExtras.find(itemAtual => itemAtual.item === item)
-        if (itemExtra) {
-        return item.includes(itemExtra.principal)
-        }
-        return false
+          const itemExtra = this.itensExtras.find(itemAtual => itemAtual.item === item)
+          if (itemExtra) {
+            return item.includes(itemExtra.principal)
+          }
+          return false
         }
         
         validarQntProduto (quantidadeProduto) {
-        if (quantidadeProduto === 0) {
-        return "Quantidade inválida!"
-        }
+          if (quantidadeProduto === 0) {
+            return "Quantidade inválida!"
+          }
         }
         
         validarPagamento (metodoDePagamento) {
-        if (!this.formasPagamento.includes(metodoDePagamento)) {
-        return "Forma de pagamento inválida!"
-        }
+          if (!this.formasPagamento.includes(metodoDePagamento)) {
+            return "Forma de pagamento inválida!"
+          }
         }
         
         calcularValorDaCompra (metodoDePagamento, itens) {
-        const validadorItens = this.validarItens(itens)
-        if (validadorItens) {
-        return validadorItens
-        }
+          const validadorItens = this.validarItensNoCarrinho(itens)
+          if (validadorItens) {
+            return validadorItens
+          }
+
         const validadorPagamento = this.validarPagamento(metodoDePagamento)
-        if (validadorPagamento) {
-        return validadorPagamento
-        }
+          if (validadorPagamento) {
+            return validadorPagamento
+          }
         
         let valorTotalPedido = 0
         let carrinhoCompras = 0
         
         for (const item of itens) {
           const produto = item.split(',')[0]
-        
-         
-        
           const validadorProduto = this.validarProdutos(produto)
           if (validadorProduto) {
             return validadorProduto
@@ -89,23 +87,20 @@ class CaixaDaLanchonete {
           if (validadorQntProduto) {
             return validadorQntProduto
           }
-        
-       
-            const temChantily = itens.some(item => item.split(',')[0] === 'chantily')
-            const temCafe = itens.some(item => item.split(',')[0] === 'cafe')
-            const temQueijo = itens.some(item => item.split(',')[0] === 'queijo')
-            const temSanduiche = itens.some(item => item.split(',')[0] === 'sanduiche')
-            if ((temChantily && !temCafe || (temQueijo && !temSanduiche))) {
-              return 'Item extra não pode ser pedido sem o principal'
-            }
-
+    
+          const temChantily = itens.some(item => item.split(',')[0] === 'chantily')
+          const temCafe = itens.some(item => item.split(',')[0] === 'cafe')
+          const temQueijo = itens.some(item => item.split(',')[0] === 'queijo')
+          const temSanduiche = itens.some(item => item.split(',')[0] === 'sanduiche')
+          if ((temChantily && !temCafe || (temQueijo && !temSanduiche))) {
+            return 'Item extra não pode ser pedido sem o principal'
+          }
         }
         
         valorTotalPedido = this.testaItensEPagamento(metodoDePagamento, valorTotalPedido, carrinhoCompras)
         return `R$ ${valorTotalPedido.toFixed(2).replace('.', ',')}`
     }
         
-    
     testaItensEPagamento = (metodoDePagamento, valorTotalPedido) =>{
         if (metodoDePagamento === 'dinheiro'){
             return valorTotalPedido * this.desconto
@@ -115,6 +110,5 @@ class CaixaDaLanchonete {
         return valorTotalPedido
     }
 }
-
 
 export { CaixaDaLanchonete };
