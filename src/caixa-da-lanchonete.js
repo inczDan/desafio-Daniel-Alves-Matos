af4fb38a1d0df05b748b6a956ec054752be95a3e
inczDan <daniel.alvmatos@gmail.com>
2023-08-15 01:13:40 -0300

    feat: passa em todos testesgit add .git add .

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -2,101 +2,109 @@ class CaixaDaLanchonete {
 
     constructor () {
         this.cardapio = {
-            cafe : 3.00,
-            chantily : 1.50,
-            suco : 6.20,
-            sanduiche : 6.50,
-            queijo : 2.00,
-            combo1 : 9.50,
-            combo2 : 7.50
+        cafe : 3.00,
+        chantily : 1.50,
+        suco : 6.20,
+        sanduiche : 6.50,
+        queijo : 2.00,
+        combo1 : 9.50,
+        combo2 : 7.50
         }
         this.itensExtras = [
-            {item: 'chantily', principal: 'cafe'},
-            {item: 'queijo', principal: 'sanduiche'}
+        {item: 'chantily', principal: 'cafe'},
+        {item: 'queijo', principal: 'sanduiche'}
         ]
-        this.formasPagamento = ['dinheiro', 'credito', 'debito'] 
-        this.desconto = 0.95   
+        this.formasPagamento = ['dinheiro', 'credito', 'debito']
+        this.desconto = 0.95
         this.taxa = 1.03
-    }
-
-    validarItens = (itens) => {
-        if (itens.length === 0){
-            return "Não há itens no carrinho de compra!"
         }
-    }
-    
-
-    validarProdutos = (produto) => {
-        if (produto.length < 2 || !(produto in this.cardapio)){
-            return "Item inválido!"
+        
+        validarItens (itens) {
+        if (itens.length === 0) {
+        return "Não há itens no carrinho de compra!"
         }
-    }
-
-    validarItensExtras = (item) => {
+        }
+        
+        validarProdutos (produto) {
+        if (produto.length < 2 || !(produto in this.cardapio)) {
+        return "Item inválido!"
+        }
+        }
+        
+        validarItensExtras (item) {
         return this.itensExtras.some(itemAtual => itemAtual.item === item)
-    }
-
-    validarPrincipalPresente(item){
+        }
+        
+        validarItemPrincipal (item) {
         const itemExtra = this.itensExtras.find(itemAtual => itemAtual.item === item)
-        if(itemExtra){
-            return itens.includes(itemExtra.principal)
+        if (itemExtra) {
+        return item.includes(itemExtra.principal)
         }
         return false
-    }
-
-    validarQntProduto = (quantidadeProduto) => {
-        if (quantidadeProduto === 0){
-            return `Quantidade inválida!`
         }
-    }
-    
-
-    validarPagamento = (metodoDePagamento) => {
-        if (!this.formasPagamento.includes(metodoDePagamento)){
-            return "Forma de pagamento inválida!"
+        
+        validarQntProduto (quantidadeProduto) {
+        if (quantidadeProduto === 0) {
+        return "Quantidade inválida!"
         }
-    }
-
-
-    calcularValorDaCompra(metodoDePagamento, itens) {
-       const validadorItens = this.validarItens(itens)
-        if(validadorItens){
-            return validadorItens
+        }
+        
+        validarPagamento (metodoDePagamento) {
+        if (!this.formasPagamento.includes(metodoDePagamento)) {
+        return "Forma de pagamento inválida!"
+        }
+        }
+        
+        calcularValorDaCompra (metodoDePagamento, itens) {
+        const validadorItens = this.validarItens(itens)
+        if (validadorItens) {
+        return validadorItens
         }
         const validadorPagamento = this.validarPagamento(metodoDePagamento)
-        if(validadorPagamento){
-            return validadorPagamento
+        if (validadorPagamento) {
+        return validadorPagamento
         }
-
+        
         let valorTotalPedido = 0
         let carrinhoCompras = 0
         
-        for(const item of itens){
-            const produto = item.split(',')[0]
+        for (const item of itens) {
+          const produto = item.split(',')[0]
+        
+         
+        
+          const validadorProduto = this.validarProdutos(produto)
+          if (validadorProduto) {
+            return validadorProduto
+          }
+        
+          if (this.validarItensExtras(item)) {
+          return 'Item extra não pode ser pedido sem o principal'
+          }
 
-            if(this.validarItensExtras(item)){
-                if(!this.validarPrincipalPresente(produto)){
-                    return 'Item extra não pode ser pedido sem o principal'
-                }
+          const quantidadeProduto = Number(item.slice(item.length - 1))
+          carrinhoCompras += quantidadeProduto
+          valorTotalPedido += this.cardapio[produto] * quantidadeProduto
+          const validadorQntProduto = this.validarQntProduto(quantidadeProduto)
+          if (validadorQntProduto) {
+            return validadorQntProduto
+          }
+        
+       
+            const temChantily = itens.some(item => item.split(',')[0] === 'chantily')
+            const temCafe = itens.some(item => item.split(',')[0] === 'cafe')
+            const temQueijo = itens.some(item => item.split(',')[0] === 'queijo')
+            const temSanduiche = itens.some(item => item.split(',')[0] === 'sanduiche')
+            if ((temChantily && !temCafe || (temQueijo && !temSanduiche))) {
+              return 'Item extra não pode ser pedido sem o principal'
             }
 
-            const validadorProduto = this.validarProdutos(produto)
-            if(validadorProduto){
-                return validadorProduto
-            }
-
-            const quantidadeProduto = Number(item.slice(item.length-1))
-            carrinhoCompras += quantidadeProduto
-            valorTotalPedido += this.cardapio[produto] * quantidadeProduto 
-            const validadorQntProduto = this.validarQntProduto(quantidadeProduto)
-            if(validadorQntProduto){
-                return validadorQntProduto
-            }
         }
+        
         valorTotalPedido = this.testaItensEPagamento(metodoDePagamento, valorTotalPedido, carrinhoCompras)
-        return `R$ ${valorTotalPedido.toFixed(2).replace('.',',')}`;
+        return `R$ ${valorTotalPedido.toFixed(2).replace('.', ',')}`
     }
-
+        
     
     testaItensEPagamento = (metodoDePagamento, valorTotalPedido) =>{
         if (metodoDePagamento === 'dinheiro'){
